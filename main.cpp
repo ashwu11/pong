ff2cf108a3faa874eac38bf806b5573a5f0ac262
Ashley Wu <ashleywu2004@gmail.com>
2024-01-07 14:15:52 -0800

    Draw paddles and the ball

--- main.cpp
+++ main.cpp
@@ -9,14 +9,20 @@ int main()
 {
     cout << "Starting the game!" << endl;
 
-    const int WIDTH = 1280;
+    const int WIDTH = 1200;
     const int HEIGHT = 800;
+    const int GAP = 10;
+    
     InitWindow(WIDTH, HEIGHT, "My Pong Game");
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false) {
         BeginDrawing();
 
+        DrawLine(WIDTH/2, 0, WIDTH/2, HEIGHT, WHITE);
+        DrawCircle(WIDTH/2, HEIGHT/2, 20, WHITE);
+        DrawRectangle(GAP, HEIGHT/2 - 60, 25, 120, WHITE);
+        DrawRectangle(WIDTH - 25 - GAP, HEIGHT/2 - 60, 25, 120, WHITE);
 
         EndDrawing();
     }
